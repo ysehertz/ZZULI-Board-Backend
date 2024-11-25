3f1fbd83acedfc074e4c5ff9e71d7d947918d411
ljq <143599399+ysehertz@users.noreply.github.com>
2024-11-26 03:00:15 +0800

    new-config-redis的配置文件

---
+++ src/main/java/zzuli/config/RedisConfiguration.java
@@ -0,0 +1,39 @@
+package zzuli.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * ClassName: RedisConfiguration
+ * Package: com.sky.config
+ * Description:
+ *
+ * @author fuchen
+ * @version 1.0
+ * @createTime 2024/5/30
+ */
+@Configuration
+@Slf4j
+public class RedisConfiguration {
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory factory) {
+        log.info("创建RedisTemplate对象");
+        RedisTemplate redisTemplate = new RedisTemplate();
+        // 设置redis连接工厂对象
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        //设置value的序列化方式json，使用GenericJackson2JsonRedisSerializer替换默认序列化
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
